af68ea48084cf1b1d15beaaf031443ab4947bf35
Yurii Zubar <barsuk0831@gmail.com>
2021-01-16 17:40:28 +0200

    1

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -46,64 +46,6 @@ export default function ContactForm({ onSubmit }) {
   );
 }
 
-// class ContactForm extends Component {
-//   state = {
-//     name: '',
-//     number: '',
-//   };
-// static propTypes = {
-//   name: PropTypes.string,
-//   number: PropTypes.number,
-// };
-
-// handleInputChange = e => {
-//   const { name, value } = e.currentTarget;
-//   this.setState({ [name]: value });
-// };
-
-// handleSubmit = e => {
-//   e.preventDefault();
-
-//   this.props.onSubmit({ ...this.state, id: shortid.generate() });
-//   this.reset();
-// };
-
-// reset = () => {
-//   this.setState({
-//     name: '',
-//     number: '',
-//   });
-// };
-
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit} className={s.form}>
-//         <label className={s.name}>Name</label>
-//         <input
-//           className={s.inputName}
-//           type="text"
-//           name="name"
-//           value={this.state.name}
-//           autoComplete="off"
-//           onChange={this.handleInputChange}
-//         />
-//         <label className={s.number}>Number</label>
-//         <input
-//           className={s.inputNumber}
-//           type="tel"
-//           name="number"
-//           value={this.state.number}
-//           autoComplete="off"
-//           onChange={this.handleInputChange}
-//         />
-//         <button type="submit" className={s.btn}>
-//           Add contact
-//         </button>
-//       </form>
-//     );
-//   }
-// }
-
 ContactForm.propTypes = {
   name: PropTypes.string,
   number: PropTypes.number,
