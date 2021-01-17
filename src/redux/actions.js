a13c0362e0826b3577b499401d7070bf0181ce31
Yurii Zubar <barsuk0831@gmail.com>
2021-01-17 10:59:10 +0200

    create redux

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,16 +1,16 @@
-import shortid from 'shortid';
-import { createAction } from '@reduxjs/toolkit';
+// import shortid from 'shortid';
+// import { createAction } from '@reduxjs/toolkit';
 
-const addContacts = createAction('pnoneBook/add', data => ({
-  payload: {
-    id: shortid.generate(),
-    data,
-  },
-}));
+// const addContacts = createAction('pnoneBook/add', data => ({
+//   payload: {
+//     id: shortid.generate(),
+//     data,
+//   },
+// }));
 
-const deleteContacts = createAction('pnoneBook/delete');
-const changeFilter = createAction('pnoneBook/changeFilter');
+// const deleteContacts = createAction('pnoneBook/delete');
+// const changeFilter = createAction('pnoneBook/changeFilter');
 
-const actions = { addContacts, deleteContacts, changeFilter };
+// const actions = { addContacts, deleteContacts, changeFilter };
 
-export default actions;
+// export default actions;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,38 +1,46 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import logger from 'redux-logger';
+// import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+// import logger from 'redux-logger';
+// import phoneBookReducer from './reducer';
+
+// const middleware = [
+//   ...getDefaultMiddleware({
+//     serializableCheck: {
+//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//     },
+//   }),
+//   logger,
+// ];
+
+// const phoneBookPersistConfig = {
+//   key: 'contacts',
+//   storage,
+//   blacklist: ['filter'],
+// };
+// export const store = configureStore({
+//   reducer: {
+//     phoneBook: persistReducer(phoneBookPersistConfig, phoneBookReducer),
+//   },
+//   middleware,
+//   devTools: process.env.NODE_ENV === 'development',
+// });
+
+// export const persistor = persistStore(store);
+
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import phoneBookReducer from './reducer';
 
-const middleware = [
-  ...getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
-  logger,
-];
+const store = createStore(phoneBookReducer, composeWithDevTools());
 
-const phoneBookPersistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter'],
-};
-export const store = configureStore({
-  reducer: {
-    phoneBook: persistReducer(phoneBookPersistConfig, phoneBookReducer),
-  },
-  middleware,
-  devTools: process.env.NODE_ENV === 'development',
-});
-
-export const persistor = persistStore(store);
+export default store;
